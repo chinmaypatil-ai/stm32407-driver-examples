ba40a3a22490b8b8212b717f916e1ff61a683deb
Chinmay Patil <154702941+chinmaypatil-ai@users.noreply.github.com>
2025-09-19 00:03:57 +0530

    Fix comment formatting and spelling in LED_Toggle.c

---
+++ LED_Toggle.c
@@ -0,0 +1,71 @@
+/*
+ * 001ledtoogle.c
+ * Normal Pull up pull down confrigation
+ *  Created on: Sep 18, 2025
+ *      Author: Chinmay
+ */
+
+
+#include "stm32f407xx.h"
+
+void delay(void)
+{
+	for(uint32_t i = 0; i < 500000 ; i++);
+}
+
+int main(void)
+{
+	GPIO_Handle_t GpioLed;
+
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_Handle_t GpioLed1;
+
+	GpioLed1.pGPIOx = GPIOD;
+	GpioLed1.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GpioLed1.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed1.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed1.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed1.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+
+	GPIO_Handle_t GpioLed2;
+
+	GpioLed2.pGPIOx = GPIOD;
+	GpioLed2.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	GpioLed2.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed2.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed2.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed2.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+
+	GPIO_Handle_t GpioLed3;
+
+	GpioLed3.pGPIOx = GPIOD;
+	GpioLed3.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GpioLed3.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed3.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed3.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed3.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+	GPIO_Init(&GpioLed1);
+	GPIO_Init(&GpioLed2);
+	GPIO_Init(&GpioLed3);
+
+	while(1)
+	{
+		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+		delay();
+	}
+  return 0;
+}
